b6c2d572fb4a0f86c9e839a7a4325bf376586d5e
DaliGabriel <21015405@es.uveg.edu.mx>
2022-05-25 12:33:58 -0500

    db connected :)

---
+++ models/conexion.php
@@ -0,0 +1,13 @@
+<?php
+
+
+/*Get ip adress from a dns
+Resolve-DnsName -Name {dpg-ca74hvsobjd80oe3a8i0-a.oregon-postgres.render.com}
+*/
+$dbconn = pg_connect("host=35.227.164.209 dbname=crud_dyxv user=slapfi password=JKhIurpIEb0nSJu4EzOQaHjkStmgrT3b");  
+//connect to a database named "postgres" on the host "host" with a username and password  
+if ($dbconn){  
+echo "correcto";  
+}else{  
+ echo "no funciono";  
+}
