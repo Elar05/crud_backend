3728062a015d8e9aa6d5d241edf6a0f1c037fee9
leonardoVillao <leo.villao@yahoo.com>
2022-05-29 22:02:51 -0500

    actualizacion pagina CREATE

--- ajax/create.php
+++ ajax/create.php
@@ -1,12 +1,18 @@
 <?php require_once './models/clientesModel.php';
-
-if (!isset($_POST["nombre"]) || !isset($_POST["edad"])) {
-  exit();
-} else {
+$ok = "";
+$error = "";
+if ((isset($_POST["nombre"]) && !empty($_POST['nombre']))  || (isset($_POST["edad"]) && !empty($_POST['edad']))) {
   $nombre = $_POST["nombre"];
   $edad = $_POST["edad"];
 
   $res = new ClientesModel();
   $sentencia = $base_de_datos->prepare("INSERT INTO mascotas(nombre, edad) VALUES (?, ?);");
   $resultado = $sentencia->execute([$nombre, $edad]);
+  $ok = $resultado;
+}else{
+  $error = "0-Campo Nombre y Edad Obligatorio";
 }
+$n = new stdClass();
+$n->error = $error;
+$n->ok = $ok;
+echo json_encode($n);
--- index.php
+++
@@ -1,5 +0,0 @@
-<?php
-require_once 'models/Database.php';
-$r = new Database();
-
-var_dump($r->getCon());
